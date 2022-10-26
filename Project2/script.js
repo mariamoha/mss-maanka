2510af0a72a70e7bfef138a509d0792eb15e0b2f
mariamoha <muhimhaji8@gmail.com>
2022-10-26 02:21:24 -0700

    waxaan kusoo dari rabaa sawiro iyo video

---
+++ Project2/script.js
@@ -0,0 +1,9 @@
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// menu.addEventlistener("click" , function () {
+//    navbar.classlist.toggle("active");
+// });
+// window.onscroll = () => {
+//     navbar.classlist.remove("active");
+// };
